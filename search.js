0af421aa73f276c3f977f7825db192304045485f
shedivesin <>
2022-11-16 06:25:10 -0600

    clean up

--- search.js
+++ search.js
@@ -1,149 +1,178 @@
 "use strict";
 
-// ## GEOMETRY
+function round(x) { return Math.round(x * 1e8); }
 
-function distance(a, b) {
-  return Math.hypot(b[0] - a[0], b[1] - a[1]);
+function hash1(x) {
+  x = round(x);
+  if(!(x >= -2147483648 && x < 2147483648)) {
+    throw new RangeError("Number went out of range for hashing");
+  }
+
+  return String.fromCharCode(
+    x >>> 24,
+    (x >>> 16) & 255,
+    (x >>> 8) & 255,
+    x & 255,
+  );
 }
 
+function hash2(x, y) { return hash1(x) + hash1(y); }
+
+function hash3(x, y, z) { return hash1(x) + hash1(y) + hash1(z); }
+
+function contains_hash2(points, x2, y2) {
+  const n = points.length;
+  const h2 = hash2(x2, y2);
+
+  for(let i = 0; i < n; i++) {
+    const p1 = points[i];
+    const h1 = hash2(p1[0], p1[1]);
+    if(h1 === h2) { return true; }
+  }
+
+  return false;
+}
+
+function contains_hash3(circles, x2, y2, r2) {
+  const n = circles.length;
+  const h2 = hash3(x2, y2, r2);
+
+  for(let i = 0; i < n; i++) {
+    const c1 = circles[i];
+    const h1 = hash3(c1[0], c1[1], c1[2]);
+    if(h1 === h2) { return true; }
+  }
+
+  return false;
+}
+
+function distance(x1, y1, x2, y2) { return Math.hypot(x2 - x1, y2 - y1); }
+
 // http://paulbourke.net/geometry/circlesphere/
-function intersect(p, q) {
-  const d = distance(p, q);
-  if(d > p[2] + q[2] || d < Math.abs(p[2] - q[2])) { return []; }
+function intersect(x1, y1, r1, x2, y2, r2) {
+  x2 -= x1;
+  y2 -= y1;
 
-  const a = (p[2] * p[2] - q[2] * q[2] + d * d) / (2 * d);
-  if(a >= p[2]) { return []; }
+  const d = Math.hypot(x2, y2);
+  if(d >= r1 + r2 || d <= Math.abs(r1 - r2)) { return []; }
 
-  const h = Math.sqrt(p[2] * p[2] - a * a);
+  const a = (r1 * r1 - r2 * r2 + d * d) / (2 * d);
+  const h_sq = r1 * r1 - a * a;
+  if(h_sq <= 0) { return []; }
+
+  const h = Math.sqrt(r1 * r1 - a * a);
+  x2 /= d;
+  y2 /= d;
+
   return [
-    [
-      p[0] + (q[0] - p[0]) * (a / d) - (q[1] - p[1]) * (h / d),
-      p[1] + (q[1] - p[1]) * (a / d) + (q[0] - p[0]) * (h / d),
-    ],
-    [
-      p[0] + (q[0] - p[0]) * (a / d) + (q[1] - p[1]) * (h / d),
-      p[1] + (q[1] - p[1]) * (a / d) - (q[0] - p[0]) * (h / d),
-    ],
+    [x1 + x2 * a - y2 * h, y1 + y2 * a + x2 * h],
+    [x1 + x2 * a + y2 * h, y1 + y2 * a - x2 * h],
   ];
 }
 
-function* intersection_points(list) {
-  yield [0, 0];
-  yield [1, 0];
+function hash_construction(circles) {
+  const n = circles.length;
+  if(n === 0) { return ""; }
+  if(n === 1) { return hash3(0, 0, circles[0][2]); }
 
-  for(let a = list; a; a = a[3]) {
-    for(let b = a[3]; b; b = b[3]) {
-      const points = intersect(a, b);
-      yield* points;
-    }
-  }
-}
-
-function len(list) {
-  let n = 0;
-  for(let a = list; a; a = a[3]) { n++; }
-  return n;
-}
-
-function round(x) {
-  return Math.round(x * 1e8) / 1e8;
-}
-
-// FIXME: I'm certain we can do a lot better here...
-function str(x, y, r) {
-  return round(x) + "," + round(y) + "," + round(r);
-}
-
-function hash(list, o, m00, m10, m01, m11) {
-  const strs = [];
-
-  for(let a = list; a; a = a[3]) {
-    const x = a[0] - o[0];
-    const y = a[1] - o[1];
-    const r = a[2];
-    strs.push(str(x * m00 + y * m10, x * m01 + y * m11, r));
-  }
-
-  return strs.sort().join(";");
-}
-
-function canonical_hash(list) {
-  if(!list) { return ""; }
-  if(!list[3]) { return hash(list, list, 1, 0, 0, 1); }
-
+  const temp = new Array(n);
   let best;
 
-  for(let a = list; a; a = a[3]) {
-    for(let b = list; b; b = b[3]) {
-      if(a === b) { continue; }
+  for(let i = 0; i < n; i++) {
+    const c1 = circles[i];
 
-      const d = distance(a, b);
-      const cos = (b[0] - a[0]) / d;
-      const sin = (a[1] - b[1]) / d;
+    for(let j = 0; j < n; j++) {
+      if(i === j) { continue; }
 
-      const g = hash(list, a, cos, -sin, sin, cos);
-      if(best === undefined || g.localeCompare(best) < 0) { best = g; }
+      const c2 = circles[j];
+      const d = distance(c1[0], c1[1], c2[0], c2[1]);
+      if(!round(d)) { continue; }
 
-      const h = hash(list, a, cos, -sin, -sin, -cos);
-      if(best === undefined || h.localeCompare(best) < 0) { best = h; }
+      const cos = (c2[0] - c1[0]) / d;
+      const sin = (c1[1] - c2[1]) / d;
+
+      for(let s = 1; s >= -1; s -= 2) {
+        for(let k = 0; k < n; k++) {
+          const c3 = circles[k];
+          const x = c3[0] - c1[0];
+          const y = c3[1] - c1[1];
+          const r = c3[2];
+          temp[k] = hash3(x * cos - y * sin, (x * sin + y * cos) * s, r);
+        }
+
+        const hash = temp.sort().join("");
+        if(best === undefined || hash.localeCompare(best) < 0) { best = hash; }
+      }
     }
   }
 
   return best;
 }
 
-function contains(list, x) {
-  for(let a = list; a; a = a[3]) {
-    if(str(a[0], a[1], a[2]) === x) {
-      return true;
-    }
-  }
+function search_to_depth(test, circles, points, visited, depth) {
+  if(circles.length >= depth) { return false; }
 
-  return false;
-}
+  const hash = hash_construction(circles);
+  if(visited.has(hash)) { return false; }
+  visited.add(hash);
 
-function search(depth) {
-  const start = Date.now();
-  const open = [];
-  // FIXME: A bloom filter would be much more space-efficient.
-  const closed = new Set();
+  if(test(circles, points)) { return true; }
 
-  open.push(null);
-  closed.add(canonical_hash(null));
+  const m = circles.length;
+  const n = points.length;
+  let done = false;
 
-  while(open.length) {
-    const curr = open.shift();
+  for(let i = 0; i < n; i++) {
+    const p1 = points[i];
 
-    // FIXME: We could cache length on each node, making this step O(1).
-    if(len(curr) >= depth) { continue; }
+    for(let j = 0; j < n; j++) {
+      if(i === j) { continue; }
 
-    for(const p of intersection_points(curr)) {
-      for(const q of intersection_points(curr)) {
-        const r = distance(p, q);
-        if(round(r) === 0 || contains(curr, str(p[0], p[1], r))) { continue; }
+      const p2 = points[j];
+      const r = distance(p1[0], p1[1], p2[0], p2[1]);
+      if(!round(r) || contains_hash3(circles, p1[0], p1[1], r)) { continue; }
 
-        const next = [p[0], p[1], r, curr];
-        const hash = canonical_hash(next);
-        if(closed.has(hash)) { continue; }
+      circles.push([p1[0], p1[1], r]);
 
-        open.push(next);
-        closed.add(hash);
+      for(let k = 0; k < m; k++) {
+        const c1 = circles[k];
+        for(const p of intersect(c1[0], c1[1], c1[2], p1[0], p1[1], r)) {
+          if(contains_hash2(points, p[0], p[1])) { continue; }
+          points.push(p);
+        }
       }
+
+      done = search_to_depth(test, circles, points, visited, depth) || done;
+
+      circles.length = m;
+      points.length = n;
     }
   }
 
-  console.log(
-    "Explored %d state(s) in %d ms.",
-    closed.size,
-    Date.now() - start,
-  );
+  return done;
 }
 
-search(0);
-search(1);
-search(2);
-search(3);
-search(4);
-search(5);
-search(6);
-search(7);
+function search(test) {
+  const start = Date.now();
+  const circles = [];
+  const points = [[0, 0], [1, 0]];
+  const visited = new Set();
+
+  for(
+    let depth = 0;
+    !search_to_depth(test, circles, points, visited, depth);
+    visited.clear(), depth++
+  );
+
+  const end = Date.now();
+  console.log("Explored %d state(s) in %d ms.", visited.size, end - start);
+}
+
+search(({length}) => length >= 0);
+search(({length}) => length >= 1);
+search(({length}) => length >= 2);
+search(({length}) => length >= 3);
+search(({length}) => length >= 4);
+search(({length}) => length >= 5);
+search(({length}) => length >= 6);
+//search(({length}) => length >= 7);
